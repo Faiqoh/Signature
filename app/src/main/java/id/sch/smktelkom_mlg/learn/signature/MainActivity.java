fcf3272eafadeeb09773ed7253c98c4b5c6e13bc
Faiqoh <faiqoh11.fa@gmail.com>
2017-08-02 15:46:49 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/signature/MainActivity.java
@@ -0,0 +1,31 @@
+package id.sch.smktelkom_mlg.learn.signature;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnadmin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+
+        findViewById(R.id.btncs).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main3Activity.class));
+            }
+        });
+
+
+    }
+}
